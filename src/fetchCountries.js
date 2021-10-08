eb8ed2c4c49e7e66c385b10d2af6b4a429fc475f
DValeriy <valeriydm20@gmail.com>
2021-10-08 14:01:17 +0300

    done

---
+++ src/fetchCountries.js
@@ -0,0 +1,47 @@
+import config from "./config.json"
+
+// export const fetchCountries = (name) => {
+//     return fetch(`${config.baseUrlStart}${name}${config.baseUrlEnd}`)
+//     .then((response) => {
+//       if (response.ok) return response.json()
+//       return response.json().then(res => Promise.reject(res));
+//       // if (!response.ok)  throw new Error(`is not ok: ` +response.status);
+//       // if (!response.ok)  return Promise.reject(`Oops, there is no country with that name`);
+//       // throw new Error(response.status);
+//       ;
+//     })
+//     // .then(data => console.log(data))
+//   .catch(error =>  Notiflix.Notify.failure(`Oops, there is no country with that name`));
+// };
+
+export const fetchCountries = (name) => {
+    return fetch(`${config.baseUrlStart}${name}${config.baseUrlEnd}`)
+    .then((response) => {
+      if (!response.ok) {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+            }
+            return response.json();
+        })
+};
+
+
+// fetch(url)
+//     .then(resp => (resp.ok)
+//         ? resp.json()
+//         : Promise.reject('is not ok: ' + resp.status)
+//     )
+//     .catch((err) => {
+//         console.warn(err)
+//     })
+
+
+// fetch(url)
+//     .then(resp => {
+//         if (!resp.ok) {
+//             throw Error(`is not ok: ` + resp.status);
+//         }
+//     return resp.json();
+//     })
+//     .catch((err) => {
+//         console.warn(err)
+//     })
---
+++ src/index.js
@@ -0,0 +1,79 @@
+import './css/styles.css';
+import { fetchCountries } from "./fetchCountries";
+import debouce from "lodash.debounce";
+import Notiflix from 'notiflix';
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+// Notiflix.Notify.warning('Memento te hominem esse');
+
+// Notiflix.Notify.info('Cogito ergo sum');
+
+const DEBOUNCE_DELAY = 300;
+
+const nodes = {
+  inputSearch: document.querySelector("#search-box"),
+  countryListNode:document.querySelector(".country-list"),
+  countryInfoNode:document.querySelector(".country-info")
+}
+const showContent = (arrCountry) => {
+  const markup = arrCountry.map(({ name, flag }) =>
+    `<li class="cards">
+      <img class="flag" src="${flag}  "/>
+      <div ">${name}</div>
+    </li>`
+  )
+    .join("");
+  nodes.countryListNode.innerHTML = markup;
+}
+const showCountryInfo = (country) => {
+  const markup = country.map(({languages,flag,name ,capital,population}) => {
+    let arrLang = [];
+    for (const i of languages)
+      arrLang.push(i.name);
+    
+    let str=`<li class="country">
+      <img class="flag" src="${flag}"/>
+      <ul class="card">
+      <li>Name: ${name}</li>
+      <li>Capital: ${capital}</li>
+      <li>Population: ${population}</li>
+      <li>Languages: ${arrLang.join(",")}</li>
+      </ul>`
+    return str;
+  }
+    )
+  nodes.countryInfoNode.innerHTML = markup;
+}
+
+
+const handlerForInput = (e) => {
+  const valueOfInput = nodes.inputSearch.value.trim();
+  if (!valueOfInput) {
+    nodes.countryInfoNode.innerHTML = "" ;
+    nodes.countryListNode.innerHTML = "";
+  }
+  if (valueOfInput) { 
+    fetchCountries(valueOfInput)
+      .then(data => {
+        console.log(data)
+        if (data.length > 10) {
+          nodes.countryInfoNode.innerHTML = "" ;
+          nodes.countryListNode.innerHTML = "";
+          Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+        }
+        if (data.length <= 10 && data.length >= 2) {
+          nodes.countryInfoNode.innerHTML = "" ;
+          showContent(data)
+        }
+        if (data.length === 1) {
+          nodes.countryListNode.innerHTML = "";
+          showCountryInfo(data)
+        }
+      } )
+      .catch(error => Notiflix.Notify.failure(`Oops, there is no country with that name`));
+  }
+};
+nodes.inputSearch.addEventListener("input", debouce(handlerForInput, DEBOUNCE_DELAY));
